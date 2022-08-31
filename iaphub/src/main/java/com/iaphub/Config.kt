c754e65bb7eeee79b0bccf6068e18cf96c3a89a5
Max <maxime@iaphub.com>
2022-08-30 21:20:11 -0500

    Version update to 3.1.0

--- iaphub/src/main/java/com/iaphub/Config.kt
+++ iaphub/src/main/java/com/iaphub/Config.kt
@@ -8,7 +8,7 @@ internal object Config {
   // SDK platform
   val sdk = "android"
   // SDK version
-  val sdkVersion = "3.0.1"
+  val sdkVersion = "3.1.0"
   // Cache version (Increment it when cache needs to be reset because of a format change)
   val cacheVersion = "1"
 }
