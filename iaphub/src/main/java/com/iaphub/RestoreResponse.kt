f6273502e8e1e31576bc3d8ae601ce68976b9fa0
Max <maxime@iaphub.com>
2022-12-16 05:44:07 +0100

    Remove internal on RestoreResponse properties

--- iaphub/src/main/java/com/iaphub/RestoreResponse.kt
+++ iaphub/src/main/java/com/iaphub/RestoreResponse.kt
@@ -3,9 +3,9 @@ package com.iaphub
 class RestoreResponse {
 
   // New purchases
-  internal val newPurchases: List<ReceiptTransaction>
+  val newPurchases: List<ReceiptTransaction>
   // Existing active products transferred to the user
-  internal val transferredActiveProducts: List<ActiveProduct>
+  val transferredActiveProducts: List<ActiveProduct>
 
   constructor(newPurchases: List<ReceiptTransaction>, transferredActiveProducts: List<ActiveProduct>) {
     this.newPurchases = newPurchases
